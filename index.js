a7b94c261bd04b4c637bd5e1321e58f5b73fd647
robbiesh <sherrard.rob@gmail.com>
2021-02-04 09:47:32 -0500

    on ready

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import jQuery from "jquery";
 
-jQuery(".js-add-to-cart").click(function () {
-  jQuery.ajax({
-    type: "POST",
-    url: "/cart/add.js",
-    data: {
-      quantity: 1,
-      id: "37671584661681",
-    },
-    dataType: "json",
-    success: function (data) {
-      alert("success!");
-    },
-  });
-});
+jQuery(document).ready(function () {
+    console.log("we are here!");
+
+    jQuery(".js-add-to-cart").click(function () {
+      jQuery.ajax({
+        type: "POST",
+        url: "/cart/add.js",
+        data: {
+          quantity: 1,
+          id: "37671584661681",
+        },
+        dataType: "json",
+        success: function (data) {
+          alert("success!");
+        },
+      });
+    });
+
+})
