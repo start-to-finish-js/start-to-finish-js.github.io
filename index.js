fc5d51c6b42ad870a20d6e0d74501667828ae819
robbiesh <sherrard.rob@gmail.com>
2021-02-04 11:16:29 -0500

    reload page

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 import jQuery from "jquery";
 
-
 jQuery(document).ready(function () {
   const urlParams = new URLSearchParams(window.location.search);
   const preload = urlParams.get("preload");
 
   if (preload) {
     const variants = preload.split(",");
-
-    for (let variant of variants) {
-      jQuery.ajax({
-        type: "POST",
-        url: "/cart/add.js",
-        data: {
-          quantity: 1,
-          id: variant,
-        },
-        dataType: "json",
-        success: function (data) {
-          console.log("Successfully added: " + variant);
-        },
-      });
-    }
+    addVariants(variants);
   }
 });
+
+function addVariants(variants) {
+  var promises = [];
+  for (let variant of variants) {
+    var request = jQuery.ajax({
+      type: "POST",
+      url: "/cart/add.js",
+      data: {
+        quantity: 1,
+        id: variant,
+      },
+      dataType: "json",
+      success: function (data) {
+        console.log("Successfully added: " + variant);
+      },
+    });
+    promises.push(request);
+  }
+
+  $.when.apply(null, promises).done(function () {
+    window.location.href = window.location.href.split("?")[0];
+  });
+}
