972ba053f96078f057271bb958f118da620205a8
robbiesh <sherrard.rob@gmail.com>
2021-02-05 09:24:46 -0500

    quantity

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ jQuery(document).ready(function () {
   const urlParams = new URLSearchParams(window.location.search);
   const preload = urlParams.get("preload");
 
+  console.log("are we here");
+
   if (preload) {
     const variants = preload.split(",");
     addVariants(variants);
@@ -13,16 +15,24 @@ jQuery(document).ready(function () {
 function addVariants(variants) {
   var promises = [];
   for (let variant of variants) {
+    let variant_array = variant.split(":");
+
+    let quantity = variant_array.length > 1 ? variant_array[1] : 1;
+
     var request = jQuery.ajax({
       type: "POST",
       url: "/cart/add.js",
       data: {
-        quantity: 1,
-        id: variant,
+        id: variant_array[0],
+        quantity: quantity,
       },
       dataType: "json",
       success: function (data) {
         console.log("Successfully added: " + variant);
+      },
+      fail: function (e) {
+        console.log("Failure!");
+        console.log(e);
       },
     });
     promises.push(request);
